05b75809e590ea1927efcdf79b783aaea402d9e2
davemoz <davejmoz@gmail.com>
2020-06-26 17:42:57 -0400

    Test sub-cat logic

--- inc/kmc-functions/woocommerce-functions.php
+++ inc/kmc-functions/woocommerce-functions.php
@@ -419,7 +419,6 @@ if ( ! function_exists( 'kmc_woocommerce_output_product_categories' ) ) {
 			$cat_parent = get_category( $category )->parent;
 			$cat_count = get_category( $category )->count;
 			if( $cat_parent != 0 && $cat_count > 1 ) {
-				echo "Not 0 && > 1.";
 				wc_get_template(
 					'content-product_cat.php',
 					array(
@@ -428,49 +427,7 @@ if ( ! function_exists( 'kmc_woocommerce_output_product_categories' ) ) {
 				);
 			}
 			elseif( $cat_parent != 0 && $cat_count <= 1 ) {
-				echo "Not 0 && <= 1.";
-				if ( woocommerce_product_loop() ) {
-
-					/**
-					 * Hook: woocommerce_before_shop_loop.
-					 *
-					 * @hooked woocommerce_output_all_notices - 10
-					 * @hooked woocommerce_result_count - 20
-					 * @hooked woocommerce_catalog_ordering - 30
-					 */
-					do_action( 'woocommerce_before_shop_loop' );
-
-					woocommerce_product_loop_start();
-
-					if ( wc_get_loop_prop( 'total' ) ) {
-						while ( have_posts() ) {
-							the_post();
-
-							/**
-							 * Hook: woocommerce_shop_loop.
-							 */
-							do_action( 'woocommerce_shop_loop' );
-
-							wc_get_template_part( 'content', 'product' );
-						}
-					}
-
-					woocommerce_product_loop_end();
-
-					/**
-					 * Hook: woocommerce_after_shop_loop.
-					 *
-					 * @hooked woocommerce_pagination - 10
-					 */
-					do_action( 'woocommerce_after_shop_loop' );
-				} else {
-					/**
-					 * Hook: woocommerce_no_products_found.
-					 *
-					 * @hooked wc_no_products_found - 10
-					 */
-					do_action( 'woocommerce_no_products_found' );
-				}
+				echo "Parent cat == true. Prod count <= 1.";
 			}
 			elseif( $cat_parent == 0 ) {
 				echo "Category parent is: " . $cat_parent . ".";
