a996bf15fbfd9f3e6afc3296ca0b88a8eece7fd8
davemoz <davejmoz@gmail.com>
2020-06-26 17:29:38 -0400

    Change cat parent ID check to 0

--- inc/kmc-functions/woocommerce-functions.php
+++ inc/kmc-functions/woocommerce-functions.php
@@ -418,7 +418,7 @@ if ( ! function_exists( 'kmc_woocommerce_output_product_categories' ) ) {
 			$cat_name = get_category( $category )->name;
 			$cat_parent = get_category( $category )->parent;
 			$cat_count = get_category( $category )->count;
-			if( $cat_parent != null && $cat_count > 1 ) {
+			if( $cat_parent == 0 && $cat_count > 1 ) {
 				wc_get_template(
 					'content-product_cat.php',
 					array(
@@ -426,7 +426,7 @@ if ( ! function_exists( 'kmc_woocommerce_output_product_categories' ) ) {
 					)
 				);
 			}
-			elseif( $cat_parent != null && $cat_count <= 1 ) {
+			elseif( $cat_parent == 0 && $cat_count <= 1 ) {
 				if ( woocommerce_product_loop() ) {
 
 					/**
